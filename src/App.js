169661732d0fe080069e21d2c778548187328ce6
Andrey <55235312+andrew133@users.noreply.github.com>
2022-01-28 16:28:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,279 @@
+// import React from 'react';
+// import './App.css';
+//
+// В коде в разных местах допущена одна и та же ошибка.
+//
+//     Какое слово должно быть написано вместо ошибочного?
+//
+//     copy
+// export const Wrapper  = () => {
+//     return <user city="minsk" />
+// }
+// type PropsType = {
+//     city: string
+// }
+// export const user: React.FC<PropsType> = (props) => {
+//     return <div>hello</div>
+// }
+// Что нужно написать вместо ххх, чтобы на экране увидеть:
+//
+//     ? Заголовок видео
+//
+// ? Контент видео
+//
+// ? Статистика лайков
+//
+// copy
+// import ReactDOM from 'react-dom'
+//
+// export const YoutubeVideo = () => {
+//     return <div>
+//         <VideoHeader />
+//         <VideoContent />
+//         <VideoStatistics />
+//     </div>
+// }
+// export const VideoHeader = () => {
+//     return <div>
+//         ? Заголовок видео
+//     </div>
+// }
+// export const VideoContent = () => {
+//     return <div>
+//         ? Контент видео
+//     </div>
+// }
+// export const VideoStatistics = () => {
+//     return <div>
+//         ? Статистика лайков
+//     </div>
+// }
+//
+// ReactDOM.render(<div>ххх</div>,
+//     document.getElementById('root')
+// );
+//
+// //Что нужно написать вместо ххх, чтобы на экране увидеть:
+// //? Заголовок видео
+// //? Контент видео
+// //? Статистика лайков
+// Что нужно написать вместо ххх, что бы код работал?
+//
+//     copy
+//     type PropsType = {
+//     name: string
+//     description: string
+// }
+// export const User: React.FC<PropsType> = (props) => {
+//     return <div>
+//         <h1>Имя: {props.name}</h1>
+//         //<div>Описание: {ххх}</div>
+//     </div>
+// }
+// Что МИНИМАЛЬНО ДОСТАТОЧНО нужно дописать в строке 8 (cтрока с ошибкой), чтобы не было ошибки
+//
+//
+// copy
+// type PropsType = {
+//     city: string
+//     country: string
+//     coords?: string
+// }
+// // 'belarus', 'minsk', '53.917501,27.604851'
+// export const Wrapper1 = () => {
+//     return <PropsComponent1 city='minsk'/>
+// }
+//
+// export const PropsComponent1: React.FC<PropsType> = (props) => {
+//     return <div>hello</div>
+// }
+// Что нужно написать вместо xxx и yyy? Ответ дайте через пробел, например:
+// blabla onClick(props)
+//
+// copy
+// type PagePropsType = {
+//     age: number
+//     name: string
+//     avatarUrl: string
+// }
+// const Page: React.FC<PagePropsType> = (props) => {
+//     return <User a={xxx} n={yyy} />
+// }
+// type UserPropsType = {
+//     a: number
+//     n: string
+// }
+// export const User: React.FC<UserPropsType> = (props) => {
+//     return <div>name: {props.n}, age: {props.a}</div>
+// }
+// Что нужно написать вместо XXX и YYY? Ответ дайте через пробел, например:
+// car user
+//
+//
+// copy
+// type NewsType = {
+//     title: string
+//     author: string
+// }
+// type ArticleType = {
+//     title: string
+//     date: Date
+//     text: string
+// }
+// type PagePropsType = {
+//     news: NewsType[]
+//     mainArticle: ArticleType
+// }
+// export const Page: React.FC<PagePropsType> = (props) => {
+//     return <div>
+//         <article>
+//             <h1>Название: {props.XXX.title}</h1>
+//             {props.XXX.date}
+//             <div>{props.XXX.date}</div>
+//             <div>{props.XXX.text}</div>
+//         </article>
+//         <aside>Articles:
+//             <ul>
+//                 {props.YYY.map(n => <li>{n.title}, {n.author}</li>)}
+//             </ul>
+//         </aside>
+//     </div>
+// }
+// Что нужно написать вместо xxx и yyy, чтобы компонент UserMoney отобразил информацию о двух кошельках (bitcoin и $)?
+//
+//     Ответ дайте через пробел, например: props.x wallets
+//
+// copy
+// type UserWalletType = {
+//     title: string
+//     amount: number
+// }
+// type UserWalletPropsType = {
+//     wallet: UserWalletType
+// }
+//
+// export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+//     return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+// }
+//
+// export const UserMoney = () => {
+//     const wallets = [
+//         {title: 'bitcoin', amount: 1},
+//         {title: '$', amount: 100}
+//     ]
+//
+//     return <div>
+//         <UserWallet wallet={xxx} />
+//         <UserWallet wallet={yyy} />
+//     </div>
+// }
+// Что нужно написать вместо xxx yyy zzz, чтобы увидеть ожидаемый результат?
+//
+//     Ответ дайте через пробел, пример: a={12} ccc={video.id} d={'hello'}
+//
+//
+// copy
+// import ReactDOM from 'react-dom'
+//
+// export const VideoHeader = (props: {videoName: string}) => {
+//     return <div>
+//         ? {props.videoName}
+//     </div>
+// }
+// export const VideoContent = (props: {videoContent: string}) => {
+//     return <div>
+//         ? <a href={props.videoContent}>{props.videoContent}</a>
+//     </div>
+// }
+// export const VideoDescription = (props: {videoDescription: string}) => {
+//     return <div>
+//         ? {props.videoDescription}
+//     </div>
+// }
+//
+// export const YoutubeVideo = (props: any) => {
+//     return <div>
+//         <VideoHeader xxx />
+//         <VideoContent yyy />
+//         <VideoDescription zzz />
+//     </div>
+// }
+//
+// export const App = () => {
+//     const video = {
+//         title: 'Samurai way',
+//         link: 'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8',
+//         description: 'Best free react-course'
+//     }
+//
+//     return <YoutubeVideo video={video} />
+// }
+//
+// ReactDOM.render(<App />,
+//     document.getElementById('root')
+// );
+// Что нужно написать вместо XXX YYY ZZZ? Ответ дайте через пробел
+//
+//
+// copy
+// import ReactDOM from 'react-dom'
+//
+// const CrazyButton = (props: any) => {
+//
+//     const style = {
+//         color: props.XXX,
+//         backgroundColor: props.YYY
+//     }
+//
+//     return <button style={style}>
+//         {props.ZZZ}
+//     </button>
+// }
+//
+// export const App = () => {
+//     return <div>
+//         <CrazyButton title={'delete'} fontColor={'white'} bgColor={'red'}/>
+//         <CrazyButton title={'add'} fontColor={'white'} bgColor={'green'}/>
+//     </div>
+// }
+//
+// ReactDOM.render(<App/>,
+//     document.getElementById('root')
+// )
+//
+// Что нужно написать вместо XXX YYY ZZZ? Ответ дайте через пробел
+//
+// copy
+// import ReactDOM from 'react-dom'
+//
+// const Son = (props: any) => {
+//     return <div>
+//         I am son. My name is {props.name}
+//     </div>
+// }
+//
+//
+// const Father = (props: any) => {
+//     return <div>
+//         I am father. My name is {props.name}
+//         <Son name={props.sonName} />
+//     </div>
+// }
+//
+// const Granny = (props: any) => {
+//     return <div>
+//         I am granny. My name is {props.name}
+//         <Father name={props.fatherName} sonName={props.sonName} />
+//     </div>
+// }
+//
+// export const App = () => {
+//     return <div>
+//         <Granny XXX={'Бабуля'} YYY={'Батя'} ZZZ={'Сын'}/>
+//     </div>
+// }
+//
+// ReactDOM.render(<App/>,
+//     document.getElementById('root')
+// )
+// export default App;
---
+++ src/exzem2.js
@@ -0,0 +1,223 @@
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const results = useState<any>(["Bob", "Alex", "Ann"])
+    const users = results[0]
+    const setUsers = results[1]
+
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+
+// Какой тип правильнее указать вместо "any" при типизации стэйта?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    return (
+        <p>Тут будет список пользователей!</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList />, document.getElementById('root')
+);
+
+// Что вернёт выражение: typeof useState?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const results = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    const users = results[0]
+    const setUsers = results[1]
+
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+
+// Чему равно results.length?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: Array.isArray(users)?
+
+
+
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: typeof setUsers?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                { users.ХХХ(user => <li>{user}</li> )}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо вставить вместо ХХХ, чтобы код заработал?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    const getUser = (user: string) => <li>{user}</li>
+    const usersList = users.map(XXX)
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {usersList}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо вставить вместо XXX, чтобы код заработал?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    const getUser = (user: string) => <li>{user}</li>
+    const usersList = users.map(getUser)
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {usersList}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: Array.isArray(usersList)?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    const getUser = (user: string) => <li>{user}</li>
+
+    return (
+        <main>
+            <h4>User list:</h4>
+            <ul>
+                {users.map(getUser)}
+            </ul>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: typeof getUser?
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+type UserType = {
+    id: number
+    name: string
+    age: number
+}
+
+function User(props: UserType) {
+    return (
+        <li>User {props.name}: {props.age} y.o.</li>
+    )
+}
+
+function UsersList() {
+    const state = [
+        {id: 1, name: "Bob", age: 34},
+        {id: 2, name: "Alex", age: 25},
+        {id: 3, name: "Ann", age: 30},
+        {id: 4, name: "John", age: 23},
+    ]
+    const users = [
+        {id: 1, userName: "Bob", age: 34},
+        {id: 2, userName: "Alex", age: 25},
+        {id: 3, userName: "Ann", age: 30},
+        {id: 4, userName: "John", age: 23},
+    ]
+
+    const [usersList, setUsersList] = useState<Array<UserType>>(XXX)
+    return (
+        <main>
+            <h5>User list:</h5>
+            <p>Тут будет список пользователей</p>
+        </main>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что надо написать вместо XXX, чтобы код работал?

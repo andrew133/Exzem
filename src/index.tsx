bedd6a3d7aacd85e92a830dfd192d377b4bf754f
Andrey <55235312+andrew133@users.noreply.github.com>
2022-10-06 23:01:15 +0300

    06.10

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ function Notes() {
             />
             <div>
                 <button
-                     onClick={xxxxx}
+                     onClick={xxxx123x}
                 >Clear notes list</button>
             </div>
             <h4>Notes:</h4>
